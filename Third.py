4bcfe4f678e7399aa6f0503d94c73c73d6e34d84
Bhaskar Shukla <166659705+Bhaskar440@users.noreply.github.com>
2026-05-06 22:14:57 +0000

    LOOP

--- Third.py
+++ Third.py
@@ -4,3 +4,17 @@ if __name__ == '__main__':
     print(a+b)
     print(a-b)
     print(a*b)
+
+#if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)    
+#if __name__ == '__main__': 
+## for loop is used here with range
+    n = int(input())
+    i = 0
+    for i in range(n):
+        print(i**2)    
+
+        
